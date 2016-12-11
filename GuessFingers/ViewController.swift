01b1fa1f55afb8c9e6e6798664549474348a928c
Dave Schumaker <daveschumaker@gmail.com>
2016-12-11 10:30:04 -0800

    Init commit

--- GuessFingers/ViewController.swift
+++ GuessFingers/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuess: UITextField!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBAction func guessNumber(_ sender: UIButton) {
+        let randomNumber = Int(arc4random_uniform(6))
+        let userNumber:Int? = Int(userGuess.text!)
+        
+        if (userNumber == randomNumber) {
+            statusLabel.text = "Correct! The correct number was \(randomNumber)"
+        } else {
+            statusLabel.text = "Wrong, I was showing \(randomNumber) fingers"
+        }
+    }
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ README.md
@@ -0,0 +1,4 @@
+# Guess Fingers
+
+A demo app created after following an iOS developer course on Udemy:
+https://www.udemy.com/complete-ios-10-developer-course/learn/v4/overview
